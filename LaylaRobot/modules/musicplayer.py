c50a39cb72b2ea904add56fd82bef61dbe3633ac
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-11 20:10:18 +0700

    Delete musicplayer.py

--- LaylaRobot/modules/musicplayer.py
+++
@@ -1,9 +0,0 @@
-__mod_name__ = "Music"
-
-__help__ = """
-❍ /song <song name> - download songs you want quickly
-❍ /search <query> - search videos on youtube with details
-❍ /deezer <song name> - download songs you want quickly via deezer
-❍ /saavn <song name> - download songs you want quickly via saavn
-❍ /video <song name> - download videos you want quickly
-"""
