9d2c4d00735e9a77841ccb6499d02591043642e3
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-11 20:05:19 +0700

    Update __nightmode.py

--- LaylaRobot/modules/__nightmode.py
+++ LaylaRobot/modules/__nightmode.py
@@ -129,7 +129,7 @@ async def job_close():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Evlie**"
+              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By @admin**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -140,7 +140,7 @@ async def job_close():
             logger.info(f"Unable To Close Group {chat} - {e}")
 
 #Run everyday at 12am
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(job_close, trigger="cron", hour=23, minute=59)
 scheduler.start()
 
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Masha**"
+              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By @admin**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -162,16 +162,16 @@ async def job_open():
             logger.info(f"Unable To Open Group {pro.chat_id} - {e}")
 
 # Run everyday at 06
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(job_open, trigger="cron", hour=5, minute=58)
 scheduler.start()
 
 
 __help__ = f"""
- ❍ /nightmode on/off
+ ❍ /nightmode (on/off)
  
-**Note:** Night Mode chats get Automatically closed at 12pm(IST)
-and Automatically openned at 6am(IST) To Prevent Night Spams.
+**Note:** Night Mode chats get Automatically closed at 12pm(WIB)
+and Automatically openned at 6am(WIB) To Prevent Night Spams.
 """
 
 __mod_name__ = "NightMode"
