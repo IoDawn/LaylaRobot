85c8ef33acc7000452b0306aa85c23e111a02b6a
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-12 18:33:22 +0700

    Update admin.py

--- LaylaRobot/modules/admin.py
+++ LaylaRobot/modules/admin.py
@@ -691,7 +691,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "Admin"
+__mod_name__ = "Admin⚜"
 __command_list__ = [
     "adminlist",
     "admins",
