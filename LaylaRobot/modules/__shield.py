4c081fb09618046e58edaba845aab34736f79faa
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-12 17:14:20 +0700

    Update __shield.py

--- LaylaRobot/modules/__shield.py
+++ LaylaRobot/modules/__shield.py
@@ -148,7 +148,7 @@ async def ws(event):
         await event.delete()
         st = sender.first_name
         hh = sender.id
-        final = f"**NSFW DETECTED**\n\n{st}](tg://user?id={hh}) your message contain NSFW content.. So, Layla deleted the message\n\n **Nsfw Sender - User / Bot :** {st}](tg://user?id={hh})  \n\n`⚔️Automatic Detections Powered By LaylaAI` \n**#GROUP_GUARDIAN** "
+        final = f"**NSFW DETECTED**\n\n{st}](tg://user?id={hh}) your message contain NSFW content.. So, Yone deleted the message\n\n **Nsfw Sender - User / Bot :** {st}](tg://user?id={hh})  \n\n`⚔️Automatic Detections Powered By YoneAI` \n**#GROUP_GUARDIAN** "
         dev = await event.respond(final)
         await asyncio.sleep(10)
         await dev.delete()
@@ -210,7 +210,7 @@ async def nsfw_watch(client, message):
             Escobar = midhun.id
         await client.send_message(
             message.chat.id,
-            f"**NSFW DETECTED**\n\n{hehe}'s message contain NSFW content.. So, Layla deleted the message\n\n **Nsfw Sender - User / Bot :** `{Escobar}` \n**Chat Title:** `{ctitle}` \n\n`⚔️Automatic Detections Powered By LaylaAI` \n**#GROUP_GUARDIAN** ",
+            f"**NSFW DETECTED**\n\n{hehe}'s message contain NSFW content.. So, Yone deleted the message\n\n **Nsfw Sender - User / Bot :** `{Escobar}` \n**Chat Title:** `{ctitle}` \n\n`⚔️Automatic Detections Powered By YoneAI` \n**#GROUP_GUARDIAN** ",
         )
         message.continue_propagation()
 """
@@ -367,7 +367,7 @@ async def del_profanity(event):
                     await event.delete()
                     st = sender.first_name
                     hh = sender.id
-                    final = f"**NSFW DETECTED**\n\n{st}](tg://user?id={hh}) your message contain NSFW content.. So, Layla deleted the message\n\n **Nsfw Sender - User / Bot :** {st}](tg://user?id={hh})  \n\n`⚔️Automatic Detections Powered By LaylaAI` \n**#GROUP_GUARDIAN** "
+                    final = f"**NSFW DETECTED**\n\n{st}](tg://user?id={hh}) your message contain NSFW content.. So, Yone deleted the message\n\n **Nsfw Sender - User / Bot :** {st}](tg://user?id={hh})  \n\n`⚔️Automatic Detections Powered By YoneAI` \n**#GROUP_GUARDIAN** "
                     dev = await event.respond(final)
                     await asyncio.sleep(10)
                     await dev.delete()
@@ -376,7 +376,7 @@ async def del_profanity(event):
 
 def extract_emojis(s):
     return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
-
+    
 
 @tbot.on(events.NewMessage(pattern=None))
 async def del_profanity(event):
@@ -430,14 +430,17 @@ async def del_profanity(event):
 
 __help__ = """
 *Group Guardian:*
-✪ Bot can protect your group from NSFW senders, Slag word users and also can force members to use English
-
-*Commmands*
- - /gshield [on/off] - Enable|Disable Porn cleaning
- - /globalmode [on/off] - Enable|Disable English only mode
- - /profanity [on/off] - Enable|Disable slag word cleaning
+✪ Yone can protect your group from NSFW senders, Slag word users and also can force members to use English
+*Commands*
+ - /gshield (on/off) - Enable|Disable Porn cleaning
+ - /globalmode (on/off) - Enable|Disable English only mode
+ - /profanity (on/off) - Enable|Disable slag word cleaning
  
 Note: Special credits goes to Julia project and Friday Userbot
  
 """
 __mod_name__ = "Shield"
+
+ 
+
+
