ec896e0edb0794be3ec62522e013d8c9663bd399
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-12 17:43:44 +0700

    Update truth.py

--- LaylaRobot/modules/truth.py
+++ LaylaRobot/modules/truth.py
@@ -1,9 +1,28 @@
 #Truth and Dare Plugins
 #truth plugins
 
+import html
+import random
+import LaylaRobot.modules.truth_and_dare_string as truth_and_dare_string
+from LaylaRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+@run_async
+def truth(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
+
+
 __mod_name__ = "🎴Truth"
 __help__ = """
 🏷 `/truth` : untuk kejujuran acak
 
 Jika ingin req pertanyaan kirim ke @RosoOwner_bot
 """
+
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
+
+
+dispatcher.add_handler(TRUTH_HANDLER)
