6390b619dbaf56b961592495a984d7a139650179
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-16 11:17:40 +0700

    Update __main__.py

--- LaylaRobot/__main__.py
+++ LaylaRobot/__main__.py
@@ -477,7 +477,7 @@ def get_help(update: Update, context: CallbackContext):
             chat.id,
             text,
             InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="Back", callback_data="help_back")]]
+                [[InlineKeyboardButton(text="Kembali", callback_data="help_back")]]
             ),
         )
 
