2e7fe441bd54ceba77ab116e1de843a695ae062d
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-19 13:52:30 +0700

    Update truth_and_dare.py

--- LaylaRobot/modules/truth_and_dare.py
+++ LaylaRobot/modules/truth_and_dare.py
@@ -31,6 +31,14 @@ def roso(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
 
 
+__help__ = """
+◎ /truth : untuk kejujuran acak
+◎ /dare : untuk tantang acak
+*Jika ingin req pertanyaan kirim ke @RosoOwner_bot
+"""
+
+__mod_name__ = "T&D"
+
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
 QUOTE_HANDLER = DisableAbleCommandHandler("quote", quote)
