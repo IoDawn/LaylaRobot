0bf4f76534f59fa961c27efc79e7519ce94da815
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-21 12:42:13 +0700

    Update __tools.py

--- LaylaRobot/modules/__tools.py
+++ LaylaRobot/modules/__tools.py
@@ -1,18 +1,9 @@
-__mod_name__ = "◎Tools"
+__mod_name__ = "✙Tools"
 
 __help__ = """
 *Fake Info*
  ❍ /fakegen*:* Generates Fake Information
  ❍ /picgen*:* Generate a fake pic 
-
-*Date-time-Weather*
- ❍ /time <country code>*:* Gives information about a timezone.
- ❍ /weather <city>*:* Get weather info in a particular place.
- ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
- ❍ /wttr moon*:* Get the current status of moon
- 
-*Quotly*
-- /q : reply any message with this command to create a sticker.
 
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
