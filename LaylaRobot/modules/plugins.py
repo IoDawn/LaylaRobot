fbd50f6420052926fa0e8aa41854c0475a651174
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-23 16:45:19 +0700

    Create plugins.py

---
+++ LaylaRobot/modules/plugins.py
@@ -0,0 +1,21 @@
+ADMIN = (
+    "Here is the help for the Admin module:
+
+❍ /staff: Cek daftar admin di grup anda
+
+Admins only:
+ ❍ /pin: Menyematkan pesan yang dibalas tanpa notif- tambahkan 'loud' atau 'notify' untuk memberikan notifikasi kepada anggota grup
+ ❍ /unpin: Melepas pin pesan yang saat ini disematkan
+ ❍ /invitelink: Dapatkan tautan grup
+ ❍ /promote: Promote user
+ ❍ /demote: Turunkan jabatan user
+ ❍ /title <title>: Menetapkan judul khusus untuk admin yang dipromosikan bot
+ ❍ /reload: Refresh daftar admin
+ ❍ /antispam <on/off>: Akan mengaktifkan teknologi antispam kami atau melihat pengaturan Anda saat ini.
+ ❍ /setgtitle <title>: Menetapkan judul obrolan baru di grup Anda.
+ ❍ /setgpic: Balas ke file atau foto untuk mengatur foto profil grup!
+ ❍ /delgpic: Sama seperti di atas tetapi untuk menghapus foto profil grup.
+ ❍ /setsticker: Balas ke stiker untuk menjadikannya sebagai pack stiker grup!
+ ❍ /setdescription <deskripsi>: Tetapkan deskripsi obrolan baru di grup.
+ ❍ /zombies: Temukan semua akun mati di grup Anda.
+ ❍ /zombies clean: Hapus semua akun mati dari grup Anda.",
