a469cbbcddbec31cbfdca4ee552524608f3fdde3
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-27 06:16:36 +0700

    Update plugins_manage.py

--- LaylaRobot/modules/plugins_manage.py
+++ LaylaRobot/modules/plugins_manage.py
@@ -17,7 +17,7 @@ def manage_callback(update, context):
     query = update.callback_query
     if query.data == "manage_":
         query.message.edit_text(
-            text="""📗 *Mᴀɴᴀɢᴇ-Mᴇɴᴜ*
+            text="""📒 *Mᴀɴᴀɢᴇ-Mᴇɴᴜ*
                  \n""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
