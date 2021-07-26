d0fc95581a472a6c5baa46100bc68e8c464cd289
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-26 16:09:48 +0700

    Update plugins_admin.py

--- LaylaRobot/modules/plugins_admin.py
+++ LaylaRobot/modules/plugins_admin.py
@@ -56,7 +56,7 @@ def admin_callback(update, context):
     query = update.callback_query
     if query.data == "admin_":
         query.message.edit_text(
-            text="""ㅤ"【Aᴅᴍɪɴ-Mᴇɴᴜ】*
+            text="""📕*【Aᴅᴍɪɴ-Mᴇɴᴜ】*
                  \n""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
